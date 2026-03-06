3368df4beea1712f3e175a0fdf1a0aedabdef0cf
Surya Mandadapu <mandadapu99@gmail.com>
2026-03-06 02:23:57 -0800

    feat: add CLI with serve and version commands

--- cmd/neuralforge/main.go
+++ cmd/neuralforge/main.go
@@ -1,18 +1,69 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/mandadapu/neuralforge/internal/app"
+	"github.com/mandadapu/neuralforge/internal/config"
+	"github.com/spf13/cobra"
 )
 
+var version = "0.1.0-dev"
+
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	root := &cobra.Command{
+		Use:   "neuralforge",
+		Short: "Autonomous software factory",
+	}
+
+	root.AddCommand(serveCmd())
+	root.AddCommand(versionCmd())
+
+	if err := root.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
 
-func run() error {
-	fmt.Println("neuralforge v0.1.0-dev")
-	return nil
+func serveCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "serve",
+		Short: "Start the webhook server and worker pool",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg := config.LoadFromEnv()
+
+			a, err := app.New(cfg)
+			if err != nil {
+				return err
+			}
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			sigCh := make(chan os.Signal, 1)
+			signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+			go func() {
+				<-sigCh
+				slog.Info("shutting down")
+				cancel()
+				a.Shutdown(context.Background())
+			}()
+
+			return a.Start(ctx)
+		},
+	}
+}
+
+func versionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Print version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("neuralforge %s\n", version)
+		},
+	}
 }
